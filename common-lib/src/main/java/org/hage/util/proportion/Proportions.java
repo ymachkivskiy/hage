54c5c8e3a92b79f16c446adb00095e1ee66ef7e2
Yaroslav Machkivskiy <ihoo.ir@gmail.com>
2015-11-27 01:57:13 +0100

    #32 Add common library module with implementation for proportions

---
+++ common-lib/src/main/java/org/hage/util/proportion/Proportions.java
@@ -0,0 +1,52 @@
+package org.hage.util.proportion;
+
+import com.google.common.primitives.UnsignedInteger;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.Collections.unmodifiableMap;
+import static java.util.Collections.unmodifiableSet;
+import static java.util.stream.Collectors.toMap;
+
+public final class Proportions {
+    private final Map<Countable, BigDecimal> fractionsMap;
+
+    private Proportions(Map<Countable, BigDecimal> fractionsMap) {
+        this.fractionsMap = unmodifiableMap(fractionsMap);
+    }
+
+    public Set<Countable> getElements() {
+        return unmodifiableSet(fractionsMap.keySet());
+    }
+
+    public BigDecimal getFractionFor(Countable countable) {
+        return fractionsMap.getOrDefault(countable, BigDecimal.ZERO);
+    }
+
+    public static Proportions forCountable(Collection<Countable> countable) {
+        BigInteger summary = countable
+            .stream()
+            .map(Countable::getCount)
+            .map(UnsignedInteger::bigIntegerValue)
+            .reduce(BigInteger.ZERO, BigInteger::add);
+
+        BigDecimal sum = new BigDecimal(summary);
+
+        Map<Countable, BigDecimal> fractionMap = countable
+            .stream()
+            .filter(c -> c.getCount().compareTo(UnsignedInteger.ZERO) != 0)
+            .collect(toMap(
+                (x) -> x,
+                c -> {
+                    BigDecimal bd = new BigDecimal(c.getCount().bigIntegerValue());
+                    return bd.divide(sum, 32, BigDecimal.ROUND_CEILING);
+                }
+            ));
+
+        return new Proportions(fractionMap);
+    }
+}
