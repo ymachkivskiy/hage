aa5b0c111d9e3847b0123700a2853791dbd20638
Yaroslav Machkivskiy <ihoo.ir@gmail.com>
2016-04-12 19:09:14 +0200

    add serializable interface to agent definition

--- platform/platform-components/runtime/runtime-init-data-model/src/main/java/org/hage/platform/component/runtime/init/AgentDefinition.java
+++ platform/platform-components/runtime/runtime-init-data-model/src/main/java/org/hage/platform/component/runtime/init/AgentDefinition.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.hage.platform.simulation.runtime.Agent;
 
+import java.io.Serializable;
+
 @RequiredArgsConstructor
 @ToString
 @Getter
-public class AgentDefinition {
+public class AgentDefinition implements Serializable {
     private final Class<? extends Agent> agentClass;
 }
--- platform/platform-components/runtime/runtime-init-data-model/src/main/java/org/hage/platform/component/runtime/init/ControlAgentDefinition.java
+++ platform/platform-components/runtime/runtime-init-data-model/src/main/java/org/hage/platform/component/runtime/init/ControlAgentDefinition.java
@@ -3,7 +3,9 @@ package org.hage.platform.component.runtime.init;
 import lombok.Data;
 import org.hage.platform.simulation.runtime.ControlAgent;
 
+import java.io.Serializable;
+
 @Data
-public class ControlAgentDefinition {
+public class ControlAgentDefinition implements Serializable{
     private final Class<? extends ControlAgent> clazz;
 }
