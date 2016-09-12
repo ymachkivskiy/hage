391f2899276f79a19fdbed35ab68733dd0793e35
Yaroslav Machkivskiy <ihoo.ir@gmail.com>
2016-09-12 17:45:47 +0200

    Change approach in 1d properties configurer algorithm

--- mocked-parts/mocked-model-data/src/main/java/org/hage/mocked/simdata/state/MigrationProvokeFoodPropertiesConfigurator.java
+++ mocked-parts/mocked-model-data/src/main/java/org/hage/mocked/simdata/state/MigrationProvokeFoodPropertiesConfigurator.java
@@ -2,27 +2,33 @@ package org.hage.mocked.simdata.state;
 
 import org.hage.platform.component.structure.Position;
 import org.hage.platform.component.structure.grid.Chunk;
+import org.hage.platform.component.structure.grid.Dimensions;
 import org.hage.platform.simulation.runtime.state.UnitPropertiesStateComponent;
 import org.hage.platform.simulation.runtime.state.descriptor.PropertyDescriptor;
 import org.hage.platform.simulation.runtime.state.property.ReadWriteUnitProperties;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.hage.mocked.simdata.state.Properties.FOOD;
-import static org.hage.platform.component.structure.Position.position;
-import static org.hage.platform.component.structure.grid.Dimensions.definedBy;
 
 public class MigrationProvokeFoodPropertiesConfigurator implements UnitPropertiesStateComponent {
 
     public static final long startingFoodAmount = 5_000;
-    public static final int veryGoodPositionFoodAmount = 20_000;
+    public static final int veryGoodPositionFoodAmount = 50_000;
     public static final long specialRarePriseAmount = 2_000;
 
-    public static final int repeating = 3;
+    public static final int repeating = 20;
 
-    private static final List<Position> allPositions = new ArrayList<>(new Chunk(position(0, 0, 0), definedBy(5, 5, 5)).getInternalPositions());
+    private static final List<Chunk> chunks = asList(
+        new Chunk(Position.position(0, 0, 0), Dimensions.definedBy(2, 2, 2)),
+        new Chunk(Position.position(2, 2, 2), Dimensions.definedBy(2, 3, 2)),
+        new Chunk(Position.position(0, 2, 0), Dimensions.definedBy(2, 2, 2)),
+        new Chunk(Position.position(0, 0, 2), Dimensions.definedBy(2, 2, 2)),
+        new Chunk(Position.position(2, 0, 0), Dimensions.definedBy(2, 3, 2)),
+        new Chunk(Position.position(0, 2, 2), Dimensions.definedBy(2, 2, 2))
+        );
 
     private int counter = 0;
 
@@ -40,7 +46,7 @@ public class MigrationProvokeFoodPropertiesConfigurator implements UnitPropertie
                 counter = (counter + 1) % repeating;
 
                 if (counter == 0) {
-                    if (allPositions.get(((int) (stepNumber % allPositions.size()))).equals(unitPosition)) {
+                    if (chunks.get(((int) (stepNumber % chunks.size()))).containsPosition(unitPosition)) {
                         readWriteUnitProperties.updateAndGet(FOOD, currFood -> currFood + veryGoodPositionFoodAmount);
                     }
                 }
