31a508e2856eba08f14640027ba720a2f31352a1
Yaroslav Machkivskiy <ihoo.ir@gmail.com>
2017-03-28 22:56:38 +0200

    #67 Add introductory configuration schema and unmarshler for it

---
+++ platform/platform-components/simulation-config/simulation-config-load/xml-config-unmarshal/src/main/java/org/hage/platform/component/simulationconfig/load/xml/XmlConfigStreamUnmarshaller.java
@@ -0,0 +1,47 @@
+package org.hage.platform.component.simulationconfig.load.xml;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.hage.platform.component.simulationconfig.load.xml.Config;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.InputStream;
+import java.net.URL;
+
+@Slf4j
+public class XmlConfigStreamUnmarshaller {
+
+    private static final String SCHEMA_LOCATION = "xsd/sim-config.xsd";
+
+    private final SchemaFactory SCHEMA_FACTORY = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+    public Config unmarshalConfiguration(InputStream inputStream) {
+
+        try {
+
+            URL schemaUrl = getClass().getClassLoader().getResource(SCHEMA_LOCATION);
+            Schema schema = SCHEMA_FACTORY.newSchema(schemaUrl);
+
+            Unmarshaller jaxbUnmarshaller = unmarshallerForSchema(schema);
+
+            return (Config) jaxbUnmarshaller.unmarshal(inputStream);
+
+        } catch (JAXBException | SAXException e) {
+            log.error("Xml configuration unmarshalling failed", e);
+            throw new XmlConfigurationUnmarshalException(e);
+        }
+    }
+
+    private Unmarshaller unmarshallerForSchema(Schema schema) throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(Config.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        jaxbUnmarshaller.setSchema(schema);
+        return jaxbUnmarshaller;
+    }
+}
---
+++ platform/platform-components/simulation-config/simulation-config-load/xml-config-unmarshal/src/main/java/org/hage/platform/component/simulationconfig/load/xml/XmlConfigurationUnmarshalException.java
@@ -0,0 +1,8 @@
+package org.hage.platform.component.simulationconfig.load.xml;
+
+public class XmlConfigurationUnmarshalException extends RuntimeException {
+
+    public XmlConfigurationUnmarshalException(Throwable throwable) {
+        super( throwable);
+    }
+}
